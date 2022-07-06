89a1f39496185a20e3ea17acc40fe319aab6a039
Shubham <raiandbala@outlook.com>
2022-07-07 02:20:22 +0530

    Integration Branch_Commit 1

--- LockedMe.com/src/ProtoypeLockedMe.java
+++ LockedMe.com/src/ProtoypeLockedMe.java
@@ -3,6 +3,13 @@ class ProtoypeLockedMe {
 	public static void WelcomePage(String detail) {
 		System.out.println(detail);
 	}
+	public static void SecondOption(){
+		System.out.println("We have various options for you to select : ");
+		System.out.println("1 - Add a file to the existing directory list.");
+		System.out.println("2 - Delete a user specified file from the existing directory list.");
+		System.out.println("3 - Search a user specified file from the main directory.");
+		System.out.println("So which option do you want to go with? Please provide the option no. below :");
+	}
 	public static void main(String[] args) {
 		WelcomePage("+++++++++Welcome to LockedMe!+++++++++");
 		WelcomePage("Generic details : ");
@@ -10,6 +17,8 @@ class ProtoypeLockedMe {
 		WelcomePage("2. Developed by: Shubham Raj");
 		WelcomePage("2. Developed by: Shubham Raj");
 		WelcomePage("3. Email: raiandbala@outlook.com");
+		WelcomePage("-----------------------------------------------------------------------------------");
+		SecondOption();
 	}
 
 }
