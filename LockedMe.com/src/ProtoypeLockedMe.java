ea268db18205620424cb1d2ab318f017ad78ac7c
Shubham <raiandbala@outlook.com>
2022-07-07 03:04:12 +0530

    Feature Brach_Commit 1

--- LockedMe.com/src/ProtoypeLockedMe.java
+++ LockedMe.com/src/ProtoypeLockedMe.java
@@ -1,14 +1,44 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 class ProtoypeLockedMe {
 
 	public static void WelcomePage(String detail) {
 		System.out.println(detail);
 	}
 	public static void SecondOption(){
+		Scanner scan = new Scanner(System.in);
 		System.out.println("We have various options for you to select : ");
+		System.out.println("But First kindly provide the path name including the desired filename and type below : ");
+		String Path = scan.nextLine();
+		File file = new File(Path); 
+		boolean result;  
+		
+		System.out.println("Great! Now which option do you want to go with from below? Please provide the option no.:");
 		System.out.println("1 - Add a file to the existing directory list.");
 		System.out.println("2 - Delete a user specified file from the existing directory list.");
-		System.out.println("3 - Search a user specified file from the main directory.");
-		System.out.println("So which option do you want to go with? Please provide the option no. below :");
+		System.out.println("3 - Search a user specified file from the main directory."); 
+		
+		int Option = scan.nextInt();
+		try   
+		{ 
+			result = file.createNewFile(); 
+		if(result && Option == 1)    
+		{  
+		System.out.println("file created "+file.getCanonicalPath()); 
+		}  
+		else  
+		{  
+		System.out.println("File already exist at location: "+file.getCanonicalPath());  
+		}  
+		}   
+		catch (IOException e)   
+		{  
+		System.out.println(e);
+		}
 	}
 	public static void main(String[] args) {
 		WelcomePage("+++++++++Welcome to LockedMe!+++++++++");
