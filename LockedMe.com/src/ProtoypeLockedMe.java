500782c04c9aac272077b4dd6da08c3385c7577d
Shubham <raiandbala@outlook.com>
2022-07-07 04:13:00 +0530

    Feature Branch 1_Commit 1

--- LockedMe.com/src/ProtoypeLockedMe.java
+++ LockedMe.com/src/ProtoypeLockedMe.java
@@ -3,12 +3,17 @@ class ProtoypeLockedMe {
 	public static void WelcomePage(String detail) {
 		System.out.println(detail);
 	}
+	
 	public static void SecondOption(){
 		System.out.println("We have various options for you to select : ");
 		System.out.println("1 - Add a file to the existing directory list.");
 		System.out.println("2 - Delete a user specified file from the existing directory list.");
 		System.out.println("3 - Search a user specified file from the main directory.");
 		System.out.println("So which option do you want to go with? Please provide the option no. below :");
+loop();
+	}
+	public static void loop() {
+		SecondOption();
 	}
 	public static void main(String[] args) {
 		WelcomePage("+++++++++Welcome to LockedMe!+++++++++");
