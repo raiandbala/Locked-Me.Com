bc7e4d100d2e18e55e0247f04fc5ab80e8e0c750
Shubham <raiandbala@outlook.com>
2022-07-07 00:42:11 +0530

    Master Branch_Commit 1

---
+++ LockedMe.com/src/ProtoypeLockedMe.java
@@ -0,0 +1,15 @@
+class ProtoypeLockedMe {
+
+	public static void WelcomePage(String detail) {
+		System.out.println(detail);
+	}
+	public static void main(String[] args) {
+		WelcomePage("+++++++++Welcome to LockedMe!+++++++++");
+		WelcomePage("Generic details : ");
+		WelcomePage("1. Project: LockedMe.com");
+		WelcomePage("2. Developed by: Shubham Raj");
+		WelcomePage("2. Developed by: Shubham Raj");
+		WelcomePage("3. Email: raiandbala@outlook.com");
+	}
+
+}
