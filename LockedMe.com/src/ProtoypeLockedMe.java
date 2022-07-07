b8e74277616c70d3baf7b6a0be2288295530af23
Shubham <raiandbala@outlook.com>
2022-07-07 05:34:19 +0530

    feature Branch 1_Commit 4

--- LockedMe.com/src/ProtoypeLockedMe.java
+++ LockedMe.com/src/ProtoypeLockedMe.java
@@ -25,7 +25,7 @@ class ProtoypeLockedMe {
 	}
 	public static void main(String[] args) {
 		WelcomePage("+++++++++Welcome to LockedMe!+++++++++");
-		WelcomePage("Generic details : ");
+		WelcomePage("app details : ");
 		WelcomePage("1. Project: LockedMe.com");
 		WelcomePage("2. Developed by: Shubham Raj");
 		WelcomePage("2. Developed by: Shubham Raj");
